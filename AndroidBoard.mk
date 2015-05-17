fd20f3c5c9bab7dde425cea832666dff736bf9e7
Louis Popi <theh2o64@gmail.com>
2015-05-17 18:18:22 +0200

    Build kernel inline, append and compress device trees to boot image.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,6 +1,51 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+BOARD_KERNEL_SEPARATED_DT := true
+KERNEL_DEFCONFIG := falcon_defconfig
+KERNEL_DIR := kernel
+
+-include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+
+ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DT)),true)
+
+ifeq ($(strip $(BUILD_TINY_ANDROID)),true)
+include device/qcom/common/dtbtool/Android.mk
+endif
+
+DTB_FILES := $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/msm8226-falcon*.dts)
+DTB_FILES := $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/*.dtb)
+DTB_FILE := $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call DTS_FILE,$(1))))
+ZIMG_FILE := $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call DTS_FILE,$(1))))
+KERNEL_ZIMG := $(KERNEL_OUT)/arch/arm/boot/zImage
+DTC = $(KERNEL_OUT)/scripts/dtc/dtc
+
+define append-dtb
+mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
+$(foreach d, $(DTB_FILES), \
+      $(DTC) -p 1024 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
+      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
+endef
+
+## Build and run dtbtool
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
+	$(call append-dtb)
+	#$(call pretty,"Target raw dt image: $(RAW_DTIMAGE_TARGET)")
+	#$(hide) $(DTBTOOL) -2 -o $(RAW_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	lz4 -9 < $(RAW_DTIMAGE_TARGET) > $@ || \
+		lz4c -c1 -y $(RAW_DTIMAGE_TARGET) $@
+		
+endif
 
 # include the non-open-source counterpart to this file
 -include vendor/motorola/falcon/AndroidBoardVendor.mk
