CyanogenMod/android_device_qcom_common

Bluetooth: Enable vendor specific BLE code
This adds config flag to enable BLE vendor specific features

Change-Id: I45ad68edd66f7e2f954746db3a0bff9a8df08d73

bdroid_buildcfg.h
@@ -26,6 +26,9 @@
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_DISPLAYONLY  TRUE
+#define BLE_PERIPHERAL_ADV_NAME  TRUE
 #endif

AOSParadox/android_device_motorola_falcon

bluetooth/bdroid_buildcfg.h
@@ -23,6 +23,8 @@
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #endif