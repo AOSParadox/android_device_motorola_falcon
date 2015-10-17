8052b088965737549c28af7dc7e93b6c61599a5b
Dmitry Grinberg <dmitrygr@gmail.com>
2015-10-17 15:44:13 +0200

    falcon: Provide moto camera symbols via libmoto
    Change-Id: Iab0f71bf1e18a60b9298793f6a8cc69208fb74b9

--- libmoto/Android.mk
+++ libmoto/Android.mk
@@ -2,8 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
-    moto_log.c
-LOCAL_SHARED_LIBRARIES := liblog
+    moto_log.c \
+    moto_camera.c
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libgui
 LOCAL_MODULE := libmoto
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
