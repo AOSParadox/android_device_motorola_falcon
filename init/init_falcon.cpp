677ad9313a316af057a7f12621a9be2725cc6411
Louis Popi <theh2o64@gmail.com>
2015-11-01 22:58:29 +0100

    init: Force single SIM * Whatever if you are Dual or Single, dual is broken and we will talk about it when Moto will realease something. By the time, let's keep it like this. * I'm exasperating of all the PMs with "PL0X FIX YUR SHIT NIGGA!"

--- init/init_falcon.cpp
+++ init/init_falcon.cpp
@@ -139,10 +139,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
             property_set("ro.build.fingerprint", "motorola/falcon_retbr_ds/falcon_umtsds:5.1/LPB23.13-56/58:user/release-keys");
             property_set("ro.build.product", "falcon_umtsds");
             property_set("ro.mot.build.customerid", "RETBR");
-            property_set("ro.telephony.default_network", "0,1");
-            property_set("persist.radio.multisim.config", "dsds");
-            property_set("persist.radio.dont_use_dsd", "true");
-            property_set("persist.radio.plmn_name_cmp", "1");
+            property_set("ro.telephony.default_network", "0");
+            property_set("persist.radio.multisim.config", "");
         }
     } else if (ISMATCH(radio, "0x6")) {
         /* xt1034 */
