172849971ad3b0df543a5aad2fac90492433f381
Louis Popi <theh2o64@gmail.com>
2016-03-26 17:54:08 +0100

    falcon: Move CAF TAG to device

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.hardware=qcom
 
+# CAF Branch
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.par.branch=LA.BF.1.1.3-01010-8x74.0
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/motorola/falcon/overlay
 PRODUCT_PACKAGE_OVERLAYS += device/motorola/falcon/overlay
